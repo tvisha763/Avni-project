e9bf4ef8c452ec33b30e7e6c0750fc6a8fed058a
Tvisha Choubey <76886688+tvisha763@users.noreply.github.com>
2024-10-08 01:36:33 +0000

    message

--- intro.py
+++ intro.py
@@ -1,1 +1,20 @@
+name="avni"
+age=9
+color="cream"
+likerain=True
+characteristics=[name,9,color,likerain]
 
+
+print(name)
+print(age)
+print(color)
+print(likerain)
+print(characteristics)
+
+
+age+=1
+print(age)
+
+
+age-=1
+print(age)
